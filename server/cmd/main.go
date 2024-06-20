fdb0fd7f158b0ad0977235f3197c68386466afc2
ykkssyaa <mr.ender.03@mail.ru>
2024-06-20 18:47:31 +0300

    starting server

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"github.com/ykkssyaa/DNS_Service/server/internal/gen"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/grpclog"
+	"net/http"
+)
+
+func run() error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+	err := gen.RegisterDnsServiceHandlerFromEndpoint(ctx, mux, "localhost:12201", opts)
+
+	if err != nil {
+		return err
+	}
+
+	return http.ListenAndServe(":8081", mux)
+}
 
 func main() {
-	fmt.Println("Hello World")
+	if err := run(); err != nil {
+		grpclog.Fatal(err)
+	}
 }
