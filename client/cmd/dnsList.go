6823fc256c00448fac84302433bd9007f78df195
ykkssyaa <mr.ender.03@mail.ru>
2024-06-21 14:29:37 +0300

    working with dns in client

--- client/cmd/dnsList.go
+++ client/cmd/dnsList.go
@@ -1,3 +1,6 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
 package cmd
 
 import (
@@ -12,48 +15,41 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// setHostnameCmd represents the setHostname command
-var setHostnameCmd = &cobra.Command{
-	Use:   "setHostname [hostname]",
-	Short: "Set new hostname on a machine",
-	Long:  ``,
+// dnsListCmd represents the dnsList command
+var dnsListCmd = &cobra.Command{
+	Use:   "dnsList",
+	Short: "Get a dns list of a machine",
+	Long:  `Get a dns list of a machine`,
 	Run: func(cmd *cobra.Command, args []string) {
 
 		conn, err := grpc.NewClient(consts.AddrGRPC, grpc.WithTransportCredentials(insecure.NewCredentials()))
-
 		if err != nil {
 			log.Fatalf("did not connect: %v", err)
 		}
-
 		defer conn.Close()
 		c := gen.NewDnsServiceClient(conn)
 
-		var hostname string
-		if len(args) > 0 {
-			hostname = args[0]
-		} else {
-			log.Fatalln("there are not hostname in args")
-		}
-
-		_, err = c.SetHostname(context.Background(), &gen.Hostname{Name: hostname})
+		res, err := c.ListDnsServers(context.Background(), &gen.Empty{})
 		if err != nil {
 			log.Fatalf("could not list DNS servers: %v", err)
 		}
 
-		fmt.Println("Hostname set to " + hostname)
+		for _, address := range res.Addresses {
+			fmt.Println(address)
+		}
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(setHostnameCmd)
+	rootCmd.AddCommand(dnsListCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// setHostnameCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// dnsListCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// setHostnameCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// dnsListCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- client/cmd/removeDns.go
+++ client/cmd/removeDns.go
@@ -1,3 +1,6 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
 package cmd
 
 import (
@@ -12,48 +15,46 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// setHostnameCmd represents the setHostname command
-var setHostnameCmd = &cobra.Command{
-	Use:   "setHostname [hostname]",
-	Short: "Set new hostname on a machine",
-	Long:  ``,
+// removeDnsCmd represents the removeDns command
+var removeDnsCmd = &cobra.Command{
+	Use:   "removeDns [address]",
+	Short: "remove dns address from dns list",
+	Long:  `remove dns address from dns list`,
 	Run: func(cmd *cobra.Command, args []string) {
 
 		conn, err := grpc.NewClient(consts.AddrGRPC, grpc.WithTransportCredentials(insecure.NewCredentials()))
-
 		if err != nil {
 			log.Fatalf("did not connect: %v", err)
 		}
-
 		defer conn.Close()
 		c := gen.NewDnsServiceClient(conn)
 
-		var hostname string
+		var address string
 		if len(args) > 0 {
-			hostname = args[0]
+			address = args[0]
 		} else {
-			log.Fatalln("there are not hostname in args")
+			log.Fatalln("there are not dns address in args")
 		}
 
-		_, err = c.SetHostname(context.Background(), &gen.Hostname{Name: hostname})
+		_, err = c.RemoveDnsServer(context.Background(), &gen.DNS{Address: address})
 		if err != nil {
 			log.Fatalf("could not list DNS servers: %v", err)
 		}
 
-		fmt.Println("Hostname set to " + hostname)
+		fmt.Println("DNS server removed from dns list")
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(setHostnameCmd)
+	rootCmd.AddCommand(removeDnsCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// setHostnameCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// removeDnsCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// setHostnameCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// removeDnsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
